3a9045103303c3d4a7719cd690130be5647f4bcf
Carl-June Piao <carljunepiao@gmail.com>
2021-07-20 06:51:05 +0800

    mod resto touch

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function main() {
 	
     // Show Menu on Book
     $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 600;
+        var navHeight = $(window).height() - 500;
         if ($(window).scrollTop() > navHeight) {
             $('.navbar-default').addClass('on');
         } else {
@@ -41,6 +41,40 @@ function main() {
       $(".navbar-collapse").collapse('hide');
     }
   });
+	
+  	// Portfolio isotope filter
+    $(window).load(function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+	
+    // Nivo Lightbox 
+    $('.portfolio-item a').nivoLightbox({
+            effect: 'slideDown',  
+            keyboardNav: true,                            
+        });
 
 }());
 
